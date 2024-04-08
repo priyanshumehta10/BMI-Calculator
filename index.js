42fc40808732cdd8c9500d344bf10029ab848b69
priyanshu mehta <mehtapriyanshu1028@gmail.com>
2024-04-09 01:03:52 +0530

    some additional features

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form");
+const BmiResult = document.querySelector("#BmiResult")
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const heightInput = document.querySelector('#height');
@@ -9,7 +10,7 @@ form.addEventListener("submit", (e) => {
     const weight = parseInt(weightInput.value);
 
     result.innerHTML = ''; // Clear previous result
-
+    BmiResult.innerHTML= "";
     if (height === "" || height < 0 || isNaN(height)) {
         let addResult = document.createTextNode("Please give a valid height");
         result.appendChild(addResult);
@@ -20,6 +21,18 @@ form.addEventListener("submit", (e) => {
         let calculation = (weight / ((height * height) / 10000)).toFixed(2);
         let addResult = document.createTextNode(`Your BMI is: ${calculation}`);
         result.appendChild(addResult);
+        if(calculation < 18.6){
+            let addBmiResult = document.createTextNode(`Under Weight ${calculation}`)
+            BmiResult.appendChild(addBmiResult);
+        }else if(calculation >18.6 && calculation < 24.9){
+            let addBmiResult = document.createTextNode(`Normal Weight ${calculation}`)
+            BmiResult.appendChild(addBmiResult);
+        }else if(calculation > 24.9){
+            let addBmiResult = document.createTextNode(`Over weight ${calculation}`)
+            BmiResult.appendChild(addBmiResult);
+        }
+        BmiResult
+        
     }
 
     // Clear input fields
